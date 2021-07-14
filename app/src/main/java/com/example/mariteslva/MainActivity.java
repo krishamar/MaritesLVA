f73510f404bead140d3c9a3c356b88169eb2722d
krishamar <2190168@slu.edu.ph>
2021-07-14 09:05:36 +0800

    Updated Application codes

--- app/src/main/java/com/example/mariteslva/MainActivity.java
+++ app/src/main/java/com/example/mariteslva/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.mariteslva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView messages;
     EditText userMessageInput;
-    FloatingActionButton sendMessage;
-    FloatingActionButton voiceMessage;
+    List<ResponseMessage> messageList;
+    MessageAdapter messageAdapter;
+
+    // FloatingActionButton sendMessage;
+   // FloatingActionButton voiceMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +33,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         messages =findViewById(R.id.containerChats);
         userMessageInput = findViewById(R.id.composeMessage);
+        messageList = new ArrayList<>();
 
+        messageAdapter = new MessageAdapter(messageList);
+        messages.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        messages.setAdapter(messageAdapter);
+
+        userMessageInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    ResponseMessage messages = new ResponseMessage(userMessageInput.getText().toString(),true);
+                    messageList.add(messages);
+
+                    ResponseMessage messages2 = new ResponseMessage(userMessageInput.getText().toString(),false);
+                    messageAdapter.notifyDataSetChanged();
+                }
+                return true;
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/mariteslva/MessageAdapter.java
+++ app/src/main/java/com/example/mariteslva/MessageAdapter.java
@@ -1,5 +1,6 @@
 package com.example.mariteslva;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,6 +10,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+// A class used to connect the messages to the Recycle view which is used to
+// display the list of messages of the bot and user in a vertical list and
+// can it is scrollable. It is also a class used to determine which viewholders
+// will be displayed.
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.CustomViewHolder>{
 
     class CustomViewHolder extends RecyclerView.ViewHolder{
@@ -21,34 +26,40 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.CustomVi
 
         }
 
-
     List<ResponseMessage> messageList;
     public MessageAdapter(List<ResponseMessage> messageList){
         this.messageList = messageList;
 
     }
 
-    @Override
-    public MessageAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(MessageAdapter.CustomViewHolder holder, int position) {
-
-    }
-
-    //method to check if the user is a bot and return the specific layout 
+    //method to check if the user is a bot and return the specific layout
     @Override
     public int getItemViewType(int position) {
+        int botMessages = R.layout.container_marites_message;
+        int userMessages = R.layout.container_user_messages;
+
         if(messageList.get(position).isMarites()){
-            return R.layout.container_marites_message;
+            return botMessages;
         }
-        return R.layout.container_user_messages;
+        return userMessages;
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CustomViewHolder view = new CustomViewHolder(LayoutInflater.from(parent.getContext()).inflate(viewType, parent,false));
+        return view ;
+
+    }
+
+    //a method used to pass the messages to the view holder
+    @Override
+    public void onBindViewHolder(MessageAdapter.CustomViewHolder holder, int position) {
+        holder.textView.setText(messageList.get(position).getMessage());
+
     }
 }
