0e1f698a8252773eb51b0d8b80c47f5d62c05d1d
2192011 <2192011@slu.edu.ph>
2021-07-22 11:41:32 +0800

    1. Edited AndroidManifest.xml 2. Edited build.gradle (removed and added some python packages) 3. Edited container_marites_message.xml and container_user_messages.xml (fixed chat UI) 4. Edited MainActivity.java (added some import statements, new object variables, added voiced text input, text-to-speech functionality, greetings when app is opened, and exit functionality) 5. Edited virtual_assistant.py (added and removed some import statements, removed most definitions, and added more statement processing of user input)

--- app/src/main/java/com/example/mariteslva/MainActivity.java
+++ app/src/main/java/com/example/mariteslva/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.mariteslva;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
@@ -19,16 +25,24 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final int RESULT_SPEECH=1;
     RecyclerView messages;
     EditText userMessageInput;
     List<ResponseMessage> messageList;
     MessageAdapter messageAdapter;
-
-    // FloatingActionButton sendMessage;
-   // FloatingActionButton voiceMessage;
+    FloatingActionButton voiceButton;
+    PyObject program = null;
+    ResponseMessage user;
+    ResponseMessage bot;
+    TextToSpeech tts;
+    Calendar calendar;
+    int time;
+    String[] exit = {"ingat", "ingat ka", "kitakits", "magkita tayo ulit", "sa susunod ulit", "paalam din",
+            "paalam din. balik ka", "salamat", "sana ay magkaroon ka ng magandang araw"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         messages =findViewById(R.id.containerChats);
         userMessageInput = findViewById(R.id.composeMessage);
+        voiceButton = findViewById(R.id.voiceButton);
         messageList = new ArrayList<>();
+        calendar = Calendar.getInstance();
 
         messageAdapter = new MessageAdapter(messageList, this);
         messages.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
@@ -47,23 +63,106 @@ public class MainActivity extends AppCompatActivity {
             Python.start(new AndroidPlatform(this));
         
         Python py = Python.getInstance();
-        final PyObject prog = py.getModule("virtual_assistant");
+        program = py.getModule("virtual_assistant");
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+            }
+        });
 
         userMessageInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEND){
-                    ResponseMessage messages = new ResponseMessage(userMessageInput.getText().toString(), false);
-                    messageList.add(messages);
+                    user = new ResponseMessage(userMessageInput.getText().toString(), false);
+                    messageList.add(user);
 
-                    PyObject res = prog.callAttr("chat", userMessageInput.getText().toString());
-                    System.out.println(res.toString());
-                    ResponseMessage bot = new ResponseMessage(res.toString(),true);
+                    PyObject res = program.callAttr("chat", userMessageInput.getText().toString());
+                    bot = new ResponseMessage(res.toString(),true);
                     messageList.add(bot);
                     messageAdapter.notifyDataSetChanged();
+                    tts.speak(res.toString(), TextToSpeech.QUEUE_FLUSH, null);
+
+                    if (res.toString().contains("pumupunta sa")) {
+                        String url = res.toString().replace("pumupunta sa", "");
+                        url = url.replaceAll("\\s+","");
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    }
+
+                    for(String word: exit) {
+                        if (word.equals(res.toString())) {
+                            MainActivity.this.finish();
+                            System.exit(0);
+                        }
+                    }
                 }
-                return true;
+                return false;
             }
         });
+
+        voiceButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fil-PH");
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                } catch (ActivityNotFoundException e){
+                    Toast.makeText(getApplicationContext(),"!", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        time = calendar.get(Calendar.HOUR_OF_DAY);
+        if (time < 12) {
+            bot = new ResponseMessage("Magandang Umaga!", true);
+            messageList.add(bot);
+            tts.speak("Magandang Umaga!", TextToSpeech.QUEUE_FLUSH, null);
+        } else if (time < 18) {
+            bot = new ResponseMessage("Magandang Hapon!", true);
+            messageList.add(bot);
+            tts.speak("Magandang Hapon!", TextToSpeech.QUEUE_FLUSH, null);
+        } else {
+            bot = new ResponseMessage("Magandang Gabi!", true);
+            messageList.add(bot);
+            tts.speak("Magandang Gabi!", TextToSpeech.QUEUE_FLUSH, null);
+        }
+        bot = new ResponseMessage("Si Marites po ito. Paano ko po kayo matutulungan", true);
+        messageList.add(bot);
+        messageAdapter.notifyDataSetChanged();
+        tts.speak("Si Marites po ito. Paano ko po kayo matutulungan", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_SPEECH && resultCode == RESULT_OK){
+            ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            user = new ResponseMessage(text.get(0), false);
+            messageList.add(user);
+
+            PyObject res = program.callAttr("chat", text.get(0));
+            bot = new ResponseMessage(res.toString(),true);
+            messageList.add(bot);
+            messageAdapter.notifyDataSetChanged();
+            tts.speak(res.toString(), TextToSpeech.QUEUE_FLUSH, null);
+
+            if (res.toString().contains("pumupunta sa")) {
+                String url = res.toString().replace("pumupunta sa", "");
+                url = url.replaceAll("\\s+","");
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+
+            for(String word: exit) {
+                if (word.equals(res.toString())) {
+                    MainActivity.this.finish();
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
--- app/src/main/python/virtual_assistant.py
+++ app/src/main/python/virtual_assistant.py
@@ -1,94 +1,93 @@
-import os
-from gtts import gTTS
-import speech_recognition as sr
+from bs4 import BeautifulSoup as soup
+import calendar
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-import webbrowser
 import datetime
+from pyowm import OWM
+from urllib.request import urlopen
 
-
-chat_bot = ChatBot(name='Marites', read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation',
-                                                                   'chatterbot.logic.BestMatch'],
+chat_bot = ChatBot(name='Marites', read_only=True,
+                   logic_adapters=['chatterbot.logic.MathematicalEvaluation',
+                                   'chatterbot.logic.BestMatch'],
                    database_uri=None)
 trainer = ChatterBotCorpusTrainer(chat_bot)
 trainer.train(
     "chatterbot.corpus.filipino"
 )
 
-
-def greet():
-    hour = int(datetime.datetime.now().hour)
-    if 0 <= hour < 12:
-        respond("Magandang Umaga!")
-    elif 12 <= hour < 18:
-        respond("Magandang Hapon!")
-    else:
-        respond("Magandang Gabi!")
-    respond("Si Marites po ito. Paano ko po kayo matutulungan")
-
-
-def take_command():
-    # Instance of speech recognition
-    r = sr.Recognizer()
-    # setting the voice pickup and sound settings of the sr object instance
-    r.energy_threshold = 150
-    r.dynamic_energy_threshold = True
-    # takes voice input from user
-    with sr.Microphone as source:
-        print("Nakikinig...")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-
-    # processes the voice input of user
-    try:
-        print("Inaalam...")
-        query = r.recognize_google(audio, language='fil-PH')  # Using google speech to text package
-        print(f"Sinabi ng gumagamit: {query}\n")  # User query will be printed.
-    except Exception as e:
-        print("Pakiulit po...")
-        query = "Pakiulit po"
-
-    return query
-
-
-def respond(audio):
-    print(audio)
-    audio_string = str(audio)
-    tts = gTTS(text=audio_string, lang='tl')
-    tts.save("reply.mp3")
-    os.system("mpg123 reply.mp3")
-
-
-def voice():
-    greet()
-    while True:
-        query = take_command().lower()  # converts user query into lower case
-        # Statements processing user input
-        if 'google' in query:
-            respond('Pumupunta sa google.com')
-            webbrowser.open('www.google.com')
-        elif 'facebook' in query:
-            respond('pumupunta sa facebook.com')
-            webbrowser.open('www.facebook.com')
-        elif 'messenger' in query:
-            respond('pumupunta sa messenger.com')
-            webbrowser.open('www.messenger.com')
-        else:
-            response = chat_bot.get_response(query)
-            respond(response)
-
-
 def chat(query):
-	# Statements processing user input
-    if 'google' in query:
-        respond('Pumupunta sa google.com')
-        webbrowser.open('www.google.com')
-    elif 'facebook' in query:
-        respond('pumupunta sa facebook.com')
-        webbrowser.open('www.facebook.com')
-    elif 'messenger' in query:
-        respond('pumupunta sa messenger.com')
-        webbrowser.open('www.messenger.com')
+    # Statements processing user input
+    res = chat_bot.get_response(query)
+    res = str(res)
+
+    if res == 'Panahon at temperatura':
+        place = 'Philippines'  # generalized to country=Philippines
+        api_key = "80ab4d0c29864f3e9e59691955104cb1"  # free OWM api_key
+        open_weather_map = OWM(api_key)
+        mgr = open_weather_map.weather_manager()
+        weather = mgr.weather_at_place(place).weather
+        weather_status = weather.detailed_status
+        temp = weather.temperature('celsius')
+        return ('Magkakaroon ngayon ng %s. Ang temperatura dito ay %0.2f degree celcius' %
+                (weather_status, temp['temp_min']))
+    elif res == 'balita ngayon':
+        news_url = "https://news.google.com/news/rss"
+        user = urlopen(news_url)
+        html = user.read()
+        user.close()
+        soup_html = soup(html, "html.parser")
+        news_list = soup_html.findAll("item")
+        for news in news_list[:5]:
+            return "Isang trending na balita ngayon ay: " + news.title.text
+    elif res == 'araw ngayon':
+        now = datetime.datetime.now()
+        my_date = datetime.datetime.today()
+        weekday = calendar.day_name[my_date.weekday()]
+        monthNum = now.month
+        dayNum = now.day
+        month_names = ['Enero', 'Pebrero', 'Marso', 'Abril', 'Mayo',
+                       'Hunyo', 'Hulyo', 'Agosto', 'Septyembre', 'Oktubre', 'Novbyembre',
+                       'Desyembre']
+        ordinalNumbers = ['Ika-1', 'Ika-2', 'Ika-3', 'Ika-4', 'Ika-5', 'Ika-6',
+                          'Ika-7', 'Ika-8', 'Ika-9', 'Ika-10', 'Ika-11', 'Ika-12',
+                          'Ika-13', 'Ika-14', 'Ika-15', 'Ika-16', 'Ika-17',
+                          'Ika-18', 'Ika-19', 'Ika-20', 'Ika-21', 'Ika-22',
+                          'Ika-23', 'Ika-24', 'Ika-25', 'Ika-26', 'Ika-27',
+                          'Ika-28', 'Ika-29', 'Ika-30', 'Ika-31']
+        return 'Ngayon ay ' + weekday + ' ' + ordinalNumbers[dayNum - 1] + ' ng ' + month_names[
+            monthNum - 1] + '.'
+    elif 'oras ngayon' in query:
+        time_now = datetime.datetime.now().strftime("%H:%M:%S")
+        return "Ang oras ngayon ay " + time_now
+    elif res == 'wikipedia':
+        return 'pumupunta sa https://wikipedia.com'
+    elif res == 'google':
+        return 'pumupunta sa https://google.com'
+    elif res == 'facebook':
+        return 'pumupunta sa https://www.facebook.com'
+    elif res == 'messenger':
+        return 'pumupunta sa https://messenger.com'
+    elif res == 'twitter':
+        return 'pumupunta sa https://twitter.com'
+    elif res == 'youtube':
+        return 'pumupunta sa https://youtube.com'
+    elif res == 'amazon':
+        return 'pumupunta sa https://amazon.com'
+    elif res == 'shopee':
+        return 'pumupunta sa https://shopee.ph'
+    elif res == 'lazada':
+        return 'pumupunta sa https://lazada.com'
+    elif res == 'yahoo':
+        return 'pumupunta sa https://yahoo.com'
+    elif res == 'Netflix':
+        return 'pumupunta sa https://netflix.com'
+    elif res == 'Instagram':
+        return 'pumupunta sa https://instagram.com'
+    elif res == 'Google Classroom':
+        return 'pumupunta sa https://classroom.google.com'
+    elif res == 'Tiktok':
+        return 'pumupunta sa https://tiktok.com'
+    elif res == 'ABS-CBN News':
+        return 'pumupunta sa https://abs-cbn.com'
     else:
-        res = chat_bot.get_response(query)
         return res
